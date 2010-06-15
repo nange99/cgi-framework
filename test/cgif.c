da5d3df0445eec675b6dc50ad9671be514b18c36
Pedro Kiefer <pkiefer@pd3.com.br>
2010-06-15 17:29:30 -0300

    Reformat cgif.c Add table tests

--- test/cgif.c
+++ test/cgif.c
@@ -5,116 +5,150 @@
 #include "../lib/cgi_list.h"
 #include "../lib/cgi_cookie.h"
 #include "../lib/cgi_session.h"
+#include "../lib/cgi_table.h"
 
-int handle_test (struct request *req, struct response *resp);
-int handle_list (struct request *req, struct response *resp);
-int handle_expr (struct request *req, struct response *resp);
-int handle_asdf (struct request *req, struct response *resp);
+int handle_test(struct request *req, struct response *resp);
+int handle_list(struct request *req, struct response *resp);
+int handle_expr(struct request *req, struct response *resp);
+int handle_table(struct request *req, struct response *resp);
 
 int main(int argc, char *argv[])
 {
 
 	struct url_mapping map[] = {
-		{.url = "/do.test", .handler = handle_test},
-		{.url = "/do.expr", .handler = handle_expr},
-		{.url = "/do.list", .handler = handle_list},
-		{.url = "/do.qwer", .handler = handle_asdf},
-		{.url = "/do.zxcv", .handler = handle_list}
+	                { .url = "/do.test", .handler = handle_test }, {
+	                                .url = "/do.expr",
+	                                .handler = handle_expr }, {
+	                                .url = "/do.list",
+	                                .handler = handle_list }, {
+	                                .url = "/do.table",
+	                                .handler = handle_table }, {
+	                                .url = "/do.zxcv",
+	                                .handler = handle_list } };
+
+	struct config conf = { NULL
+	/*default_error_html = "error.html";*/
 	};
 
-	struct config conf = {
-		NULL
-		/*default_error_html = "error.html";*/
-	};
-
-	cgi_servlet_init (&conf, &map, 5, NULL);
+	cgi_servlet_init(&conf, &map, 5, NULL);
 
 	return 0;
 }
 
-int handle_test (struct request *req, struct response *resp) {
+int handle_test(struct request *req, struct response *resp)
+{
 
 	double d;
 	char *str;
 
 	d = 3.1415;
 
-	if (cgi_request_get_parameter (req, "detail") != NULL) {
-		cgi_response_set_html (resp, "html/detail.html");
+	if (cgi_request_get_parameter(req, "detail") != NULL) {
+		cgi_response_set_html(resp, "html/detail.html");
 	} else {
-		cgi_response_set_html (resp, "html/error.html");
+		cgi_response_set_html(resp, "html/error.html");
 
-		cgi_response_add_parameter (resp, "teste", (char *)"valor de teste$!", CGI_STRING);
-		cgi_response_add_parameter (resp, "number", (void *)3222, CGI_INTEGER);
-		cgi_response_add_parameter (resp, "pi", (double *) &d, CGI_FLOAT);
+		cgi_response_add_parameter(resp, "teste",
+		                (char *) "valor de teste$!", CGI_STRING);
+		cgi_response_add_parameter(resp, "number", (void *) 3222,
+		                CGI_INTEGER);
+		cgi_response_add_parameter(resp, "pi", (double *) &d, CGI_FLOAT);
 	}
 
 	if (cgi_session_exists(req)) {
 		str = cgi_session_get_value(req, "user");
 
 		if (str != NULL)
-			cgi_response_add_parameter (resp, "user", str, CGI_STRING);
+			cgi_response_add_parameter(resp, "user", str,
+			                CGI_STRING);
 	}
 
-	cgi_cookie_add (req, "nome", "valor", NULL, NULL, NULL, NULL, 0);
+	cgi_cookie_add(req, "nome", "valor", NULL, NULL, NULL, NULL, 0);
 
 	return 1;
 }
 
-int handle_list (struct request *req, struct response *resp) {
+int handle_list(struct request *req, struct response *resp)
+{
 
 	int i;
 	cgi_list *l;
 	char *v;
 
-	cgi_session_init (req);
+	cgi_session_init(req);
 
-	cgi_session_add_value(req, "user", (char *)"zemeyer", CGI_STRING);
+	cgi_session_add_value(req, "user", (char *) "zemeyer", CGI_STRING);
 
-	l = cgi_list_create ();
+	l = cgi_list_create();
 
 	for (i = 0; i < 5; i++) {
-		v = malloc (sizeof (char) * 10);
-		sprintf (v, "valor %d", i);
-		cgi_list_append (l, (void *)v, CGI_STRING);
-		free (v);
+		v = malloc(sizeof(char) * 10);
+		sprintf(v, "valor %d", i);
+		cgi_list_append(l, (void *) v, CGI_STRING);
+		free(v);
 	}
 
-	cgi_list_remove (l, 3);
+	cgi_list_remove(l, 3);
 
 	/*
-	valor = cgi_list_get (l, 2, &type);
+	 valor = cgi_list_get (l, 2, &type);
 
-	if (valor != NULL) {
-		printf ("type = [%d] ==> valor = [%s]", type, (char *) valor);
+	 if (valor != NULL) {
+	 printf ("type = [%d] ==> valor = [%s]", type, (char *) valor);
+	 }
+
+	 printf ("list size = [%d]", cgi_list_size (l));
+
+	 printf ("list size = [%d]", cgi_list_size (l));
+	 */
+
+	cgi_response_add_parameter(resp, "list", l, CGI_LIST);
+
+	cgi_response_set_html(resp, "html/list.html");
+
+	return 1;
+}
+
+int handle_expr(struct request *req, struct response *resp)
+{
+
+	cgi_response_add_parameter(resp, "a", (void *) 1, CGI_INTEGER);
+	cgi_response_add_parameter(resp, "b", (void *) 1, CGI_INTEGER);
+	cgi_response_add_parameter(resp, "d", (void *) 10, CGI_INTEGER);
+
+	cgi_response_set_html(resp, "html/expr.html");
+
+	return 1;
+}
+
+int handle_table(struct request *req, struct response *resp)
+{
+	cgi_table *t;
+
+	t = cgi_table_create(5, "teste_1", "teste_2", "teste_3", "teste_4", "teste_5");
+
+	if (cgi_table_add_value(t, "teste_1", (char *) "value1", CGI_STRING)) {
+		printf ("should fail!\n");
 	}
 
-	printf ("list size = [%d]", cgi_list_size (l));
+	cgi_table_add_row(t);
 
-	printf ("list size = [%d]", cgi_list_size (l));
-	*/
+	if (cgi_table_add_value(t, "teste_1", (char *) "value1", CGI_STRING)) {
+		printf ("should not fail!\n");
+	} else {
+		printf ("inserted\n");
+	}
 
-	cgi_response_add_parameter (resp, "list", l, CGI_LIST);
+	if (cgi_table_add_value(t, "asdf", (char *) "value1", CGI_STRING)) {
+		printf ("should fail too!\n");
+	}
 
-	cgi_response_set_html (resp, "html/list.html");
+	cgi_table_add_row(t);
+	cgi_table_add_value(t, "teste_1", (char *) "value1", CGI_STRING);
+	cgi_table_add_value(t, "teste_2", (char *) "value2", CGI_STRING);
 
-	return 1;
-}
-
-int handle_expr (struct request *req, struct response *resp) {
-
-	cgi_response_add_parameter (resp, "a", (void *) 1, CGI_INTEGER);
-	cgi_response_add_parameter (resp, "b", (void *) 1, CGI_INTEGER);
-	cgi_response_add_parameter (resp, "d", (void *) 10, CGI_INTEGER);
-
-	cgi_response_set_html (resp, "html/expr.html");
-
-	return 1;
-}
-
-int handle_asdf (struct request *req, struct response *resp) {
-
-	cgi_response_set_html (resp, "html/asdf.html");
+	cgi_response_add_parameter(resp, "table", (void *)t, CGI_TABLE);
+	cgi_response_set_html(resp, "html/table.html");
 
 	return 1;
 }
--- test/run_cgi.sh
+++ test/run_cgi.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 export REQUEST_METHOD="GET"
-export PATH_INFO="/app/do.test"
+export PATH_INFO="/app/do.table"
 export QUERY_STRING="user=Pedro%20Kiefer&asdf=pedro"
 export HTTP_COOKIE="sid=a831bc800f842f1c5f4c59e66babbec554a98e50; "
 libtool --mode=execute valgrind --tool=memcheck -v --track-origins=yes --leak-check=full --show-reachable=yes ./test_cgi
-#glibtool --mode=execute ./test_cgi
+#libtool --mode=execute valgrind --tool=callgrind ./test_cgi
+#libtool --mode=execute ./test_cgi
