8da019f5e05ac5730b49671e44a1588cb836b716
Pedro Kiefer <pkiefer@pd3.com.br>
2010-06-22 14:21:30 -0300

    Add is_post and is_get functions

--- lib/cgi_servlet.h
+++ lib/cgi_servlet.h
@@ -34,7 +34,7 @@ char *cgi_url_decode(char *str);
 char *cgi_url_encode(char *str);
 
 int cgi_request_is_post(struct request *req);
-int cgi_request_is_get(struct request *req)
+int cgi_request_is_get(struct request *req);
 char *cgi_request_get_parameter(struct request *req, const char *name);
 
 void cgi_response_set_html(struct response *resp, const char *file);
